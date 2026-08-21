7d13d0c55744ab967d73a067e2c7d641a1db4a34
Pinky Gupta <pinkkygupta1@gmail.com>
2026-08-21 16:50:40 +0530

    Added user signup and login functionality

--- server.js
+++ server.js
@@ -75,11 +75,65 @@ app.post("/signup", (req, res) => {
                 <h1>Signup Successful!</h1>
                 <p>Welcome, ${name}!</p>
                 <p>Your account has been created successfully.</p>
-                <a href="/signup">Go Back</a>
+                <a href="/login">Login</a>
             `);
         });
     });
 });
+// Show login page
+app.get("/login", (req, res) =>
+{
+    res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+// Handle signin
+app.post("/login", (req, res) => {
+
+    // Get data from frontend form
+    const { email, password } = req.body;
+
+    // find user using email
+    const checkUserSql = "SELECT * FROM users WHERE email = ?";
+
+    db.query(checkUserSql, [email], (err, results) => {
+
+        // Database error
+        if (err) {
+            console.log("Error:", err);
+            return res.send("something went wrong");
+        }
+
+        // User does not exists
+        if (results.length === 0) 
+        {
+            return res.send("User not found");
+        }
+
+        // get user from db
+
+        const user = results[0];
+
+        //compare pwd
+
+        if(user.password === password)
+        {
+            console.log("user logged in successfully");
+
+            return res.send(`
+                <h1>Login Successful!</h1>
+                <p>Welcome, ${user.name}!</p>
+                <p>You have logged in successfully.</p>
+                <a href="/login">Go Back</a>
+                `)
+        }
+        else
+        {
+            return res.send("Invalid password");
+        }
+
+    });
+});
+
 
 // Start server
 app.listen(3000, () => {
